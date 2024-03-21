815f622c3978813b53d1eb082217550982c4e490
neqqowo <nikitakrupkevich.me@gmail.com>
2024-03-21 19:21:38 +0300

    prewiev
    da

--- scrollButton.js
+++
@@ -1,9 +0,0 @@
-function changeKitty(mood) {
-    var kittyImg = document.getElementById('kittyImg');
-    if (mood === 'happy') {
-      kittyImg.src = 'https://usagif.com/wp-content/uploads/gifs/dancing-cat-33.gif';
-    } else if (mood === 'sad') {
-      kittyImg.src = 'https://media.tenor.com/PGnGqfkIuLYAAAAM/cat-head.gif';
-    }
-  }
-  
